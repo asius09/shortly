6e52b67dbd2556d28ce84c138948f2217f069ca8
Bobby Tiwari <itsmeasius@gmail.com>
2025-09-11 13:31:44 +0530

    style: added card title & card description components

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,40 +1,70 @@
-"use client";
 import { cn } from "@/utils/cn";
-import React from "react";
+
+interface CardTitleProps extends React.HTMLAttributes<HTMLHeadingElement> {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export function CardTitle({ children, className, ...props }: CardTitleProps) {
+  return (
+    <h1
+      className={cn(
+        "mb-1 block text-center text-2xl leading-tight font-bold tracking-tight text-neutral-900 dark:text-neutral-100",
+        className,
+      )}
+      tabIndex={0}
+      {...props}
+    >
+      {children}
+    </h1>
+  );
+}
+
+interface CardDescriptionProps
+  extends React.HTMLAttributes<HTMLParagraphElement> {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export function CardDescription({
+  children,
+  className,
+  ...props
+}: CardDescriptionProps) {
+  return (
+    <p
+      className={cn(
+        "mb-6 block text-center text-base font-medium text-neutral-700 dark:text-neutral-300",
+        className,
+      )}
+      tabIndex={0}
+      {...props}
+    >
+      {children}
+    </p>
+  );
+}
 
 interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
   className?: string;
-  title?: string;
-  subTitle?: string;
 }
 
-export const Card = React.forwardRef<HTMLDivElement, CardProps>(
-  ({ children, title, subTitle, className, ...props }, ref) => (
+export function Card({ children, className, ...props }: CardProps) {
+  return (
     <div
-      ref={ref}
       className={cn(
-        "h-fit w-fit max-w-lg min-w-md rounded-2xl border p-5",
-        "border-neutral-200 bg-neutral-100 shadow-lg shadow-neutral-200/60",
-        "dark:border-neutral-800 dark:bg-neutral-900 dark:shadow-2xl dark:shadow-white/4",
-        "flex flex-col items-center justify-center",
+        [
+          "h-fit w-fit max-w-lg min-w-md rounded-2xl border p-5",
+          "border-neutral-200 bg-neutral-100 shadow-lg shadow-neutral-200/60",
+          "dark:border-neutral-800 dark:bg-neutral-900 dark:shadow-2xl dark:shadow-white/4",
+          "flex flex-col items-center justify-center",
+        ].join(" "),
         className,
       )}
       {...props}
     >
-      {title && (
-        <h2 className="mb-1 block text-center text-2xl leading-tight font-bold tracking-tight text-neutral-900 dark:text-neutral-100">
-          {title}
-        </h2>
-      )}
-      {subTitle && (
-        <p className="mb-6 block text-center text-base font-medium text-neutral-700 dark:text-neutral-300">
-          {subTitle}
-        </p>
-      )}
       {children}
     </div>
-  ),
-);
-
-Card.displayName = "Card";
+  );
+}
